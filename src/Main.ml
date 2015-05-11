9da9d1d3294853822ba531916ed1a153208cf41d
Ben Spar <sparbenjamin@gmail.com>
2015-05-11 18:05:45 -0400

    everything

---
+++ src/Main.ml
@@ -0,0 +1,157 @@
+(******************************************************************************)
+(* PipeCheck: Specifying and Verifying Microarchitectural                     *)
+(* Enforcement of Memory Consistency Models                                   *)
+(*                                                                            *)
+(* Copyright (c) 2015 Daniel Lustig, Princeton University                     *)
+(* All rights reserved.                                                       *)
+(*                                                                            *)
+(* This library is free software; you can redistribute it and/or              *)
+(* modify it under the terms of the GNU Lesser General Public                 *)
+(* License as published by the Free Software Foundation; either               *)
+(* version 2.1 of the License, or (at your option) any later version.         *)
+(*                                                                            *)
+(* This library is distributed in the hope that it will be useful,            *)
+(* but WITHOUT ANY WARRANTY; without even the implied warranty of             *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU          *)
+(* Lesser General Public License for more details.                            *)
+(*                                                                            *)
+(* You should have received a copy of the GNU Lesser General Public           *)
+(* License along with this library; if not, write to the Free Software        *)
+(* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  *)
+(* USA                                                                        *)
+(******************************************************************************)
+
+open Printf
+open Unix
+open Arg
+open PipeGraph
+open HerdGraph
+
+(* FIXME works in OCaml v4.00 - 4.01 only!
+   <  v4.00: no iteri
+   >= v4.02: strings no longer mutable *)
+let rec stringOfString s =
+  let s' = String.make (length s) '-' in
+  List.iteri (fun n c -> String.set s' n c) s;
+  s'
+
+let rec printString oc s =
+  match s with
+  | h::t -> fprintf oc "%s" (stringOfString h); printString oc t
+  | [] -> ();;
+
+let rec printStrings oc s =
+  match s with
+  | h::t -> printString oc h; printStrings oc t
+  | [] -> ();;
+
+let input_filename = ref ""
+let output_filename = ref ""
+let procname = ref "fiveStageL1OnlyProcessor"
+let graphstyle = ref "compressed"
+let do_reduction = ref true
+let model = ref "x86tso.cat"
+let keep_pruned = ref 1
+
+let update_input_filename s = input_filename := s
+let update_output_filename s = output_filename := s
+let update_procname s = procname := s
+let update_graphstyle s = graphstyle := s
+let unknown_argument s = raise (Arg.Bad ("Unknown argument: " ^ s))
+let update_model s = model := s
+let update_keep_pruned s = keep_pruned := int_of_string s
+
+let parse_anon s =
+  if (String.length !input_filename = 0)
+  then input_filename := s
+  else if (String.length !output_filename = 0)
+  then output_filename := s
+  else unknown_argument s
+
+let speclist = [
+  ("-i", Arg.String update_input_filename, "Input litmus test file");
+  ("-o", Arg.String update_output_filename, "Output file");
+  ("-p", Arg.String update_procname, "Processor");
+  ("-g", Arg.String update_graphstyle, "Graph Style");
+  ("-m", Arg.String update_model, "herd model to use (default x86tso.cat)");
+  ("-r", Arg.Clear  do_reduction, "Don't perform transitive reduction");
+  ("-k", Arg.String update_keep_pruned, "Keep pruned (-k 1)/invalid (-k 2) graphs")];;
+
+Arg.parse speclist parse_anon "PipeGraph";;
+
+let _ = if (String.length !input_filename = 0)
+  then (Arg.usage speclist "PipeGraph";
+    raise (Arg.Bad "No input file specified"))
+  else if (String.length !output_filename = 0)
+  then (Arg.usage speclist "PipeGraph";
+    raise (Arg.Bad "No output file specified"))
+  else ();;
+
+let (allowed, final_memory_values, programs) =
+  execute_herd "herd" !input_filename !model;;
+
+let processors = [
+  PipeGraph.fiveStagePipelineProcessor (length (List.hd programs));
+  PipeGraph.fiveStageViCLCreateOnlyProcessor (length (List.hd programs));
+  PipeGraph.simpleFiveStageProcessor (length (List.hd programs));
+  PipeGraph.fiveStageL1OnlyProcessor (length (List.hd programs));
+  PipeGraph.openSparcT1Processor (length (List.hd programs));
+  PipeGraph.fiveStageSSProcessor (length (List.hd programs));
+  PipeGraph.fiveStageOOOProcessor (length (List.hd programs));
+  PipeGraph.fiveStageOOONoCacheProcessor (length (List.hd programs));
+  PipeGraph.fiveStageL1OnlyOOOProcessor (length (List.hd programs));
+  PipeGraph.pitonProcessor (length (List.hd programs))
+  ]
+
+let processor =
+  let processors_list =
+    List.map (fun p -> stringOfString (PipeGraph.procName p)) processors in
+  (try
+    List.find (fun p -> stringOfString (PipeGraph.procName p) = !procname)
+    processors
+  with
+  | Not_found ->
+      Printf.printf "Unknown processor name\n";
+      Printf.printf "Known processor names are:\n";
+      List.iter (Printf.printf "\t%s\n") processors_list;
+      raise (Arg.Bad "Unknown processor"));;
+
+let program_graphs = map (PipeGraph.programGraphs !keep_pruned final_memory_values processor) programs;;
+
+Printf.printf "%d architecture-level graphs\n" (List.length program_graphs)
+
+let rec print_num_uarch_graphs l n count =
+  match l with
+  | h::t ->
+      let lh = List.length h in
+      Printf.printf "Graph %d: %d microarchitecture-level graphs\n" n lh;
+      print_num_uarch_graphs t (n+1) (count + lh)
+  | [] -> count;;
+
+let total_count = print_num_uarch_graphs program_graphs 0 0;;
+
+let outputs = if !graphstyle = "compressed"
+then map (PipeGraph.compressedGraphsOfExecutions processor !do_reduction) program_graphs
+else if !graphstyle = "original"
+then map (PipeGraph.graphsOfExecutions processor) program_graphs
+else raise (Arg.Bad "Illegal graphstyle specified");;
+
+let oc = open_out !output_filename in
+List.iter (printStrings oc) outputs;
+close_out oc;;
+
+let observable = fold_left (||) (map PipeGraph.isObservable program_graphs) false;;
+
+let result_string =
+  match (allowed, observable) with
+  | (true, true) -> "Allowed/Observable\n"
+  | (true, false) -> "Allowed/Not Observable (Stricter than necessary)\n"
+  | (false, true) -> "Forbidden/Observable (BUG!)\n"
+  | (false, false) -> "Forbidden/Not observable\n";;
+ 
+print_string result_string;;
+
+Printf.printf "Total Graphs: %d\n" total_count;
+
+print_string "Done!\n";;
+
---
+++ src/PipeGraph.v
@@ -0,0 +1,102 @@
+(******************************************************************************)
+(* Copyright (c) 2015 Daniel Lustig, Princeton University                     *)
+(*                                                                            *)
+(* Permission is hereby granted, free of charge, to any person obtaining a    *)
+(* copy of this software and associated documentation files (the "Software"), *)
+(* to deal in the Software without restriction, including without limitation  *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,   *)
+(* and/or sell copies of the Software, and to permit persons to whom the      *)
+(* Software is furnished to do so, subject to the following conditions:       *)
+(*                                                                            *)
+(* The above copyright notice and this permission notice shall be included in *)
+(* all copies or substantial portions of the Software.                        *)
+(*                                                                            *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER *)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        *)
+(* DEALINGS IN THE SOFTWARE.                                                  *)
+(******************************************************************************)
+
+Require Import Arith.
+Require Import List.
+Require Import Ascii.
+Require Import String.
+Require Import PipeGraph.Debug.
+Require Import PipeGraph.Util.
+Require Import PipeGraph.Instruction.
+Require Import PipeGraph.Processor.
+Require Import PipeGraph.Graph.
+Require Import PipeGraph.Graphviz.
+Require Import PipeGraph.GraphvizCompressed.
+Require Import PipeGraph.FiveStage.
+Require Import PipeGraph.FiveStageViCLCreateOnly.
+Require Import PipeGraph.SimpleFiveStage.
+Require Import PipeGraph.FiveStageL1Only.
+Require Import PipeGraph.OpenSparcT1.
+Require Import PipeGraph.FiveStageSuperscalar.
+Require Import PipeGraph.FiveStageOOO.
+Require Import PipeGraph.FiveStageOOONoCache.
+Require Import PipeGraph.FiveStageL1OnlyOOO.
+Require Import PipeGraph.Piton.
+
+Import ListNotations.
+
+Definition GraphsOfExecutions
+  (proc : Processor)
+  (graphs : list (NumberedAdjacencyList * MicroopPathMap))
+  : list (list string) :=
+  let f x := GraphvizGraph proc (snd x) (fst x) in
+  let g x := (EdgesFromAdjacencyList (snd (fst x)), snd x) in
+  Map f (Map g graphs).
+
+Definition CompressedGraphsOfExecutions
+  (proc : Processor)
+  (do_reduction : bool)
+  (graphs : list (NumberedAdjacencyList * MicroopPathMap))
+  : list (list string) :=
+  let f x := GraphvizCompressedGraph proc (snd x) (EdgesFromAdjacencyList (snd (fst x))) (fst (fst x)) do_reduction in
+  Map f graphs.
+
+Definition i0 := mkMicroop 0 0 0 (Write 0 1).
+Definition i1 := mkMicroop 1 0 0 (Write 1 1).
+Definition i2 := mkMicroop 2 1 0 (Read  1 1).
+Definition i3 := mkMicroop 3 1 0 (Read  0 0).
+
+Definition myProgram := [[i0; i1]; [i2; i3]].
+
+Extraction Language Ocaml.
+Extract Inductive bool => "bool" [ "true" "false" ].
+Extract Inductive list => "list" [ "[]" "(::)" ].
+Extract Inductive nat => int [ "0" "(fun x -> x + 1)" ]
+  "(fun fO fS n -> if n=0 then fO () else fS (n-1))".
+Extract Inductive ascii => char
+[
+"(* If this appears, you're using Ascii internals. Please don't *) (fun (b0,b1,b2,b3,b4,b5,b6,b7) -> let f b i = if b then 1 lsl i else 0 in Char.chr (f b0 0 + f b1 1 + f b2 2 + f b3 3 + f b4 4 + f b5 5 + f b6 6 + f b7 7))"
+]
+"(* If this appears, you're using Ascii internals. Please don't *) (fun f c -> let n = Char.code c in let h i = (n land (1 lsl i)) <> 0 in f (h 0) (h 1) (h 2) (h 3) (h 4) (h 5) (h 6) (h 7))".
+
+Extract Inductive string => "char list" [ "[]" "(::)" ].
+
+Extract Constant plus => "( + )".
+Extract Constant mult => "( * )".
+Extract Constant beq_nat => "( = )".
+
+Extract Constant Printf => "(fun x -> fun y -> let implode l = let res = String.create (List.length l) in let rec imp i = function | [] -> res | c :: l -> res.[i] <- c; imp (i + 1) l in imp 0 l in try let _ = Sys.getenv (implode ['P'; 'I'; 'P'; 'E'; 'G'; 'R'; 'A'; 'P'; 'H'; '_'; 'D'; 'E'; 'B'; 'U'; 'G']) in let rec print_chars l = match l with | h::t -> (print_char h; print_chars t) | [] -> (flush stdout; x) in print_chars y with Not_found -> x)".
+
+Extract Constant WarningHelper => "(fun x -> fun y -> let rec print_chars l = match l with | h::t -> (print_char h; print_chars t) | [] -> (flush stdout; x) in print_chars y)".
+
+(*
+Extract Constant zero => "'λ000'".
+Extract Constant one => "'λ001'".
+Extract Constant shift =>
+ "fun b c -> Char.chr (((Char.code c) lsl 1) land 255 + if b then 1 else 0)".
+Extract Inlined Constant ascii_dec => "(=)".
+*)
+Extraction "PipeGraph.ml" ProgramGraphs
+  GraphsOfExecutions CompressedGraphsOfExecutions IsObservable
+  FiveStagePipelineProcessor FiveStageViCLCreateOnlyProcessor
+  SimpleFiveStageProcessor FiveStageL1OnlyProcessor OpenSparcT1Processor FiveStageSSProcessor FiveStageOOOProcessor FiveStageOOONoCacheProcessor FiveStageL1OnlyOOOProcessor PitonProcessor myProgram.
+
